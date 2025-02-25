c0e5bca210fee19904d36920f6579092d9033aaa
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2025-02-25 16:35:50 +0100

    Update angledBackgr.vue

--- angledBackgr/angledBackgr.vue
+++ angledBackgr/angledBackgr.vue
@@ -1,1 +1,19 @@
+<template>
+  <div class="background">
+    <h2>Cool project header </h2>
+  </div>
+</template>
 
+<style scoped>
+  h2 { 
+    align-text: center;
+    color: ghostwhite; 
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+  }
+  
+.background {
+  width: 80vw;
+  height: 80vh;
+  background: linear-gradient(-20deg, #ff5733 50%, #33b5ff 50%);
+}
+</style>
