bf2db7a0a5d1bdf853683f866c9423ee5fa2c38c
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2025-02-27 11:45:39 +0100

    Update orderTckts.vue

--- order/orderTckts.vue
+++ order/orderTckts.vue
@@ -2,6 +2,7 @@
   <div id="ticket-container">
    <div id="ticket-controls">
      <button @click="ticketCount = Math.max(1, ticketCount -1)">-</button>
+         <span>{{ ticketCount }}</span>
      <button @click="ticketCount++">+</button>
   </div>
    <button @click="buyTickets" :disabled="isLoading" id="buy-button">
