8977097662a03fdb391ddca26938ce535732f1d6
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2025-02-28 18:12:31 +0100

    Update hoeveelVerkocht.vue

--- hoeveelVerkocht/hoeveelVerkocht.vue
+++ hoeveelVerkocht/hoeveelVerkocht.vue
@@ -6,6 +6,18 @@
 
 <style scoped>
 
+body { 
+  margin: 0; 
+  padding: 0; 
+  height: 100vh; 
+  width: 100%; 
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  background-color: #222; 
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; 
+  color: ghostwhite; 
+}
 
 
 </style>
